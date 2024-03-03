88480c53edbc9264a04b65bccf19df684f9d9aeb
lvsnflwrs <149216118+Lvsnflwrs@users.noreply.github.com>
2024-03-03 21:52:18 +0700

    Tambahan fitur GUI

--- TPMod3/Form1.Designer.cs
+++ TPMod3/Form1.Designer.cs
@@ -28,19 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.AcceptsTab = true;
+            textBox1.BackColor = Color.OrangeRed;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.Black;
+            textBox1.Location = new Point(88, 102);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(329, 25);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.OrangeRed;
+            button1.Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(498, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(190, 38);
+            button1.TabIndex = 1;
+            button1.Text = "BUTTON";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.OrangeRed;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(156, 267);
+            label1.Name = "label1";
+            label1.Padding = new Padding(200, 20, 200, 20);
+            label1.Size = new Size(476, 59);
+            label1.TabIndex = 2;
+            label1.Text = "OUTPUT";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
     }
 }
--- TPMod3/Form1.cs
+++ TPMod3/Form1.cs
@@ -1,3 +1,4 @@
+// Helmy Farikh Alfarizhi_1302220077
 namespace TPMod3
 {
     public partial class Form1 : Form
@@ -10,6 +11,24 @@ namespace TPMod3
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text;
+
+            // Menampilkan teks yang dimasukkan ke dalam Label
+            label1.Text = "HALO " + inputText;
         }
     }
 }
